1eb9493f0691a010c9323d199c832807f003f098
Tareq Jamil Islam <tareqjamil01910@gmail.com>
2025-07-18 16:58:18 +0600

    1

--- routes/user_routes/__init__.py
+++ routes/user_routes/__init__.py
@@ -7,7 +7,7 @@ user_routes = Blueprint("user_routes", __name__)
 @user_routes.before_request
 def check_maintenance():
     lang = request.accept_languages.best_match(["en", "bn", "ar"])
-    if os.getenv("MAINTENANCE_MODE", False):
+    if not os.getenv("MAINTENANCE_MODE", False):
         return jsonify({"action": "maintenance", "message": t("maintenance_message", lang)}), 503
 
 # Import routes from other modules to register them
